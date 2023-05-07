39a5192d60cf3a2862de86b3afd1b1c8b0755b85
mefdev <mohamedmeftah2018@gmail.com>
2023-05-07 20:59:51 +0100

    Add more functionality

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
-
- for (let index = 0; index <= 6; index++) {
-     
+for (let index = 0; index <= 6; index++) {
     document.querySelectorAll("button")[index].addEventListener("click", PlayASound);
-
-
     let keyPressed = this.innerText;
     PlayASound(keyPressed);
-    
-    
-
  };
-    
-
-
-      document.addEventListener("keypress", function (evt) {
+ document.addEventListener("keypress", function (evt) {
           PlayASound(evt.key);
-          animationWhenPress(evt.key);
+          effectWhenPress(evt.key);
       });
-    
-          
-    
-    function PlayASound(key)
+
+// play a sound with the corresponding key
+function PlayASound(key)
     {
-
-       
- 
-
          switch (key) {
              case "w":
-
                 var audio = new Audio('sounds/crash.mp3');
                 audio.play();
-                 
-
                  break;
                  case "a":
                     var audio = new Audio('sounds/kick-bass.mp3');
@@ -41,7 +23,6 @@
                  case "s":
                     var audio = new Audio('sounds/snare.mp3');
                     audio.play();
-
                 case "d":
                     var audio = new Audio('sounds/tom-1.mp3');
                     audio.play();
@@ -53,12 +34,12 @@
                 case "k":
                     var audio = new Audio('sounds/tom-3.mp3');
                     audio.play();
-            
+                break;
+                case "l":
+                    var audio = new Audio('sounds/tom-4.mp3');
+                    audio.play();
                 break;
              default:
-                var audio = new Audio('sounds/tom-4.mp3');
-                audio.play();
-                   
                  break;
          }
          
@@ -66,24 +47,14 @@
        
     }
 
-    function animationWhenPress(currentKey) {
-        var buttonPressed = document.querySelector("." + currentKey);
+// Change the look of the the button when a key is pressed
+function effectWhenPress(currentKey) {
+         let buttonPressed = document.querySelector("." + currentKey);
          buttonPressed.classList.add("pressed");
-        const delayInMilliseconds = 200; //1 second
-
- 
- 
-
-
-    setTimeout(function(){
-       
-        buttonPressed.classList.remove("pressed");
-      }, delayInMilliseconds);
-     
-     
-
- 
-
+         const delayInMilliseconds = 250; //.25 second
+         setTimeout(function(){
+            buttonPressed.classList.remove("pressed");
+         }, delayInMilliseconds);
     }
 
  
